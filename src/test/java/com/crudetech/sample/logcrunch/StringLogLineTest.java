6070d5888871a605bc35e209bfae7fafb61f56ee
Andreas Mueller <andreas.b.mueller@freenet.de>
2012-03-02 21:47:44 +0100

    -refactor

--- src/test/java/com/crudetech/sample/logcrunch/StringLogLineTest.java
+++ src/test/java/com/crudetech/sample/logcrunch/StringLogLineTest.java
@@ -21,22 +21,17 @@ public class StringLogLineTest {
 
     @Test
     public void ctorParsesInfoLevel() {
-
         assertThat(line.getLogLevel(), is("INFO"));
     }
 
     @Test
     public void ctorParsesWarnLevel() {
-        StringLogLine line = new StringLogLine("2009-06-07 13:23:57 demo.ZeroToFour main WARN: This is an informative message");
-
+        line = new StringLogLine("2009-06-07 13:23:57 demo.ZeroToFour main WARN: This is an informative message");
         assertThat(line.getLogLevel(), is("WARN"));
     }
 
     @Test
     public void ctorParsesDate() throws Exception {
-        StringLogLine line = new StringLogLine("2009-06-07 13:23:57 demo.ZeroToFour main INFO: This is an informative message");
-
-
         SimpleDateFormat frm = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         Date expected = frm.parse("2009-06-07 13:23:57");
         assertThat(line.getDate(), is(expected));
