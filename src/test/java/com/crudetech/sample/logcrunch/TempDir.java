7aad29e0c361b50f9f94de98dd44f018a70a2850
Andreas Mueller <andreas.b.mueller@freenet.de>
2012-03-05 20:40:36 +0100

    -simplified TempDir to use system properties

--- src/test/java/com/crudetech/sample/logcrunch/TempDir.java
+++ src/test/java/com/crudetech/sample/logcrunch/TempDir.java
@@ -1,32 +1,9 @@
 package com.crudetech.sample.logcrunch;
 
 import java.io.File;
-import java.io.IOException;
 
 class TempDir extends File {
     TempDir() {
-        super(tempDirName());
+        super(System.getProperty("java.io.tmpdir"));
     }
-
-    private static String tempDirName() {
-        File tmp = createTempFile("unused");
-        String tmpDir = tmp.getParent();
-        delete(tmp);
-        return  tmpDir;
-    }
-
-    private static void delete(File file) {
-        if(!file.delete()){
-            throw new RuntimeException();
-        }
-    }
-
-    private static File createTempFile(String name) {
-        try {
-            return File.createTempFile(name, null);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
