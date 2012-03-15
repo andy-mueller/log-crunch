42a1531cc6cc20837d95e8372c6963e036c34f49
Andreas Mueller <andreas.b.mueller@freenet.de>
2012-03-15 20:26:14 +0100

    -refactoring

--- src/main/java/com/crudetech/sample/logcrunch/LogFileNamePattern.java
+++ src/main/java/com/crudetech/sample/logcrunch/LogFileNamePattern.java
@@ -7,15 +7,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LogFileNamePattern {
-    private final String pattern;    //logFile.%d{yyyy-MM-dd}.log
     private static final Pattern datePattern = Pattern.compile("%d\\{.*\\}");
+    private final String[] parts;
+    private final SimpleDateFormat dateFormat;
 
     public LogFileNamePattern(String pattern) {
-        this.pattern = pattern;
+        parts = datePattern.split(pattern);
+        dateFormat = extractDateFormat(pattern);
     }
 
     public boolean matches(String fileName) {
-        String[] parts = datePattern.split(pattern);
         String comparePattern = ".*";
         for (String part : parts) {
             comparePattern += Pattern.quote(part) + ".*";
@@ -26,29 +27,28 @@ public class LogFileNamePattern {
         return matches;
     }
 
-    private SimpleDateFormat extractDateFormat() {
-        Matcher dateMatcher = datePattern.matcher(pattern);
-
+    private static SimpleDateFormat extractDateFormat(String patternString) {
+        Matcher dateMatcher = datePattern.matcher(patternString);
         if (dateMatcher.find()) {
             int start = dateMatcher.start();
             int end = dateMatcher.end();
-            String dateVal = pattern.substring(start + 3, end - 1);
+            String dateVal = patternString.substring(start + 3, end - 1);
             return new SimpleDateFormat(dateVal);
         }
         throw new IllegalArgumentException();
     }
 
     public Date dateOf(String fileName) {
-        SimpleDateFormat frm = extractDateFormat();
-        String[] parts = datePattern.split(pattern);
-        
         int start = fileName.indexOf(parts[0]) + parts[0].length();
-        int end = start + frm.toPattern().length();
-        
+        int end = start + dateFormat.toPattern().length();
         String dateString = fileName.substring(start, end);
 
+        return parseDate(dateString);
+    }
+
+    private Date parseDate(String dateString) {
         try {
-            return frm.parse(dateString);
+            return dateFormat.parse(dateString);
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
