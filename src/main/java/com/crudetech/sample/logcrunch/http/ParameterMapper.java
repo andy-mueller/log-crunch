8e6e43ffce0f6652d193b335db0fdf3a2fced614
Andreas Mueller <andreas.b.mueller@freenet.de>
2012-03-30 13:39:52 +0200

    parameter mapper for java primitives

--- src/main/java/com/crudetech/sample/logcrunch/http/ParameterMapper.java
+++ src/main/java/com/crudetech/sample/logcrunch/http/ParameterMapper.java
@@ -1,6 +1,8 @@
 package com.crudetech.sample.logcrunch.http;
 
 import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ParameterMapper {
@@ -10,16 +12,15 @@ public class ParameterMapper {
         this.parameters = parameters;
     }
 
-    public void mapTo(Object o){
+    public void mapTo(Object o) {
         Method[] methods = o.getClass().getMethods();
         for (Method method : methods) {
             Parameter par = method.getAnnotation(Parameter.class);
-            if(par == null){
+            if (par == null) {
                 continue;
             }
 
             Class<?> parameterType = getParameterType(method);
-//            parameterType.isPrimitive()
             Method factory = getFactory(parameterType);
             String parameterName = par.value();
             String[] parameterValues = gerParameterValues(parameterName);
@@ -39,7 +40,7 @@ public class ParameterMapper {
         }
     }
 
-    private Object newInstance(Method factory, String parameterValue)  {
+    private Object newInstance(Method factory, String parameterValue) {
         try {
             return setValue(factory, null, parameterValue);
         } catch (Exception e) {
@@ -49,15 +50,48 @@ public class ParameterMapper {
 
     private Class<?> getParameterType(Method method) {
         Class<?>[] parameterTypes = method.getParameterTypes();
-        if(parameterTypes.length != 1){
+        if (parameterTypes.length != 1) {
             throw new RuntimeException();
         }
-        return parameterTypes[0];
+        return convertPrimitives(parameterTypes[0]);
+    }
+
+    private static final Map<Class<?>, Class<?>> primitiveToWrapper = Collections.unmodifiableMap(new HashMap<Class<?>, Class<?>>() {
+        {
+            put(Boolean.TYPE, Boolean.class);
+            put(Integer.TYPE, Integer.class);
+            put(Short.TYPE, Short.class);
+            put(Long.TYPE, Long.class);
+            put(Character.TYPE, Character.class);
+            put(Byte.TYPE, Byte.class);
+            put(Double.TYPE, Double.class);
+            put(Float.TYPE, Float.class);
+        }
+
+        @Override
+        public Class<?> get(Object key) {
+            Class<?> nonNull = super.get(key);
+            if (nonNull == null) {
+                throw new IllegalArgumentException();
+            }
+            return nonNull;
+        }
+    });
+
+    private Class<?> convertPrimitives(Class<?> parameterType) {
+        if (!parameterType.isPrimitive()) {
+            return parameterType;
+        }
+
+        return primitiveToWrapper.get(parameterType);
     }
 
     private Method getFactory(Class<?> parameterType) {
+        Class<?> parameter = parameterType.equals(String.class)
+                             ? Object.class
+                             : String.class;
         try {
-            return parameterType.getMethod("valueOf", new Class<?>[]{String.class});
+            return parameterType.getMethod("valueOf", parameter);
         } catch (NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
--- src/test/java/com/crudetech/sample/logcrunch/http/ParameterMapperTest.java
+++ src/test/java/com/crudetech/sample/logcrunch/http/ParameterMapperTest.java
@@ -2,9 +2,12 @@ package com.crudetech.sample.logcrunch.http;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -21,6 +24,27 @@ public class ParameterMapperTest {
         class TestClass{
             private Integer item = 0;
             @Parameter("item")
+            public void setItem(Integer item){
+                this.item = item;
+            }
+        }
+
+        TestClass instance = new TestClass();
+        Map<String, String[]> parameters = new HashMap<String, String[]>();
+        parameters.put("item", new String[]{"42"});
+
+        ParameterMapper mapper = new ParameterMapper(parameters);
+
+        mapper.mapTo(instance);
+
+        assertThat(instance.item, is(42));
+    }
+
+    @Test
+    public void primitivesAreConverted(){
+        class TestClass{
+            private Integer item = 0;
+            @Parameter("item")
             public void setItem(int item){
                 this.item = item;
             }
@@ -29,10 +53,78 @@ public class ParameterMapperTest {
         TestClass instance = new TestClass();
         Map<String, String[]> parameters = new HashMap<String, String[]>();
         parameters.put("item", new String[]{"42"});
+
         ParameterMapper mapper = new ParameterMapper(parameters);
 
         mapper.mapTo(instance);
 
         assertThat(instance.item, is(42));
+    }
+    private static enum AnEnum {
+        E1, E2
+    }
+    @Test
+    public void enumsAreMapped(){
+
+        class TestClass{
+            private AnEnum item = AnEnum.E1;
+            @Parameter("anEnum")
+            public void setItem(AnEnum e){
+                this.item = e;
+            }
+        }
+
+        TestClass instance = new TestClass();
+        Map<String, String[]> parameters = new HashMap<String, String[]>();
+        parameters.put("anEnum", new String[]{"E2"});
+
+        ParameterMapper mapper = new ParameterMapper(parameters);
+
+        mapper.mapTo(instance);
+
+        assertThat(instance.item, is(AnEnum.E2));
+    }
+
+    @Test
+    public void multipleParametersValuesCallMethodForEach(){
+        class TestClass{
+            private List<Integer> items = new ArrayList<Integer>();
+            @Parameter("item")
+            public void setItem(int item){
+                items.add(item);
+            }
+        }
+
+        TestClass instance = new TestClass();
+        Map<String, String[]> parameters = new HashMap<String, String[]>();
+        parameters.put("item", new String[]{"42", "55"});
+
+        ParameterMapper mapper = new ParameterMapper(parameters);
+
+        mapper.mapTo(instance);
+
+        assertThat(instance.items, is(asList(42, 55)));
+    }
+
+    @Test
+    public void stringsAreMapped(){
+
+        class TestClass{
+            private String item = "";
+            @Parameter("aString")
+            public void setItem(String e){
+                this.item = e;
+            }
+        }
+
+        TestClass instance = new TestClass();
+        Map<String, String[]> parameters = new HashMap<String, String[]>();
+        parameters.put("aString", new String[]{"Some Text"});
+
+        ParameterMapper mapper = new ParameterMapper(parameters);
+
+        mapper.mapTo(instance);
+
+        assertThat(instance.item, is("Some Text"));
     }
 }
